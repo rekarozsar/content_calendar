1104a5d9ec293951cc516fc6b68592f51522973d
rekarozsar <rekarozsar0012@gmail.com>
2025-11-01 15:28:00 +0100

    Calendar finally loads, its in russian somehow

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { uk_UA, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import uk from '@angular/common/locales/uk';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(uk);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideNzI18n(uk_UA), provideAnimationsAsync(), provideHttpClient()
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component, signal } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { MenuOutline } from '@ant-design/icons-angular/icons';
+import { NzCalendarModule  } from 'ng-zorro-antd/calendar';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+            CommonModule,
+            NzLayoutModule, 
+            NzMenuModule,
+            NzDropDownModule,
+            NzIconModule,
+            NzCalendarModule,
+            NzCardModule, ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('content_calendar');
+  icons = [ MenuOutline ]; 
+
+  /*
+   events = new Map<string, string[]>([
+    ['2025-11-01', ['Plan social media posts', 'Team meeting']],
+    ['2025-11-03', ['Draft blog article']],
+  ]);
+
+  dateCellRender(date: Date) {
+    const key = date.toISOString().split('T')[0]; // YYYY-MM-DD
+    const dayEvents = this.events.get(key) || [];
+    return dayEvents.map(event => `<div class="event">${event}</div>`).join('');
+  }
+  */
+
+  events = new Map<string, string[]>([
+    ['2025-11-01', ['Plan social media posts', 'Team meeting']],
+    ['2025-11-03', ['Draft blog article']],
+  ]);
+
+  @ViewChild('dateCellTemplate', { static: true }) dateCellTemplate!: TemplateRef<any>;
 }
