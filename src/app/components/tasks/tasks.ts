5aedbfa24f4bbb09664323c94461a23567b88e78
rekarozsar <rekarozsar0012@gmail.com>
2026-01-08 20:09:22 +0100

    added fetchuser into user tasks logic

--- src/app/components/tasks/tasks.ts
+++ src/app/components/tasks/tasks.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [
+    AuthService
+  ],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class TasksComponent {
+  constructor(private auth: AuthService) {}
+  user: any = null;
 
+  async fetchUser() {
+    try {
+      const user = await firstValueFrom(this.auth.getUser());
+      this.user = user;
+      console.log('Fetched user:', user);
+      return user;
+    } catch (err) {
+      console.warn('Could not fetch user (not authenticated or error):', err);
+      this.user = null;
+      return null;
+    }
+  }
+
+  async ngOnInit() {
+    await this.fetchUser();
+  }
 }
