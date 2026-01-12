e0c13bab8561a41c7ae738654a304bc1e097fa2e
rekarozsar <rekarozsar0012@gmail.com>
2026-01-12 21:22:30 +0100

    added userTasks

--- src/app/components/tasks/tasks.ts
+++ src/app/components/tasks/tasks.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, map, Observable } from 'rxjs';
 import { AuthService } from '../../services/auth';
 import { TaskService } from '../../services/tasks.service';
 
@@ -13,10 +13,21 @@ import { TaskService } from '../../services/tasks.service';
   styleUrl: './tasks.css',
 })
 export class TasksComponent {
-  
+  tasks$!: Observable<any[]>;
+
   constructor(private auth: AuthService, private taskService: TaskService) {}
   user: any = null;
 
+  userTasks$!: Observable<any[]>;
+
+  ngAfterViewInit() {
+    this.userTasks$ = this.tasks$.pipe(
+      map(tasks => tasks.filter(task => 
+        task.graphics_maker === this.user?.id || task.text_writer === this.user?.id
+      ))
+    );
+    console.log('User tasks set up for user:', this.userTasks$);
+  }
   
 
   async fetchUser() {
@@ -33,6 +44,7 @@ export class TasksComponent {
   }
 
   async ngOnInit() {
+    this.tasks$ = this.taskService.tasks$;
     this.taskService.loadTasks(); 
     await this.fetchUser();
   }
