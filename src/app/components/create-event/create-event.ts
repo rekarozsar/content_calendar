e74b7a045034e41c42732d12acb802d7efa041fd
rekarozsar <rekarozsar0012@gmail.com>
2026-01-15 17:36:09 +0100

    debugging

--- src/app/components/create-event/create-event.ts
+++ src/app/components/create-event/create-event.ts
@@ -37,7 +37,7 @@ export class CreateEventComponent {
 
     // DEBUG
     console.log('EVENT graphics id:', event?.graphics_maker);
-    console.log(event);
+    console.log('event from create-events.ts',event);
     console.log('EVENT text id:', event?.text_writer);
     console.log('USERS:', this.users.map(u => u.id));
 
